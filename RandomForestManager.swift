c0cb01e20aa6d71d5bb5a885be24c71c1b9cf5c5
William Henderson <william.c.henderson@gmail.com>
2016-04-04 21:54:38 -0700

    refactor RandomForestManager so it can be run retroactively

--- RandomForestManager.swift
+++ RandomForestManager.swift
@@ -13,11 +13,26 @@ class RandomForestManager {
     var classLables: [Int32]!
     var classCount = 0
     
-    init(sampleSize: Int, samplingFrequency: Int) {
+    struct Static {
+        static var onceToken : dispatch_once_t = 0
+        static var sharedManager : RandomForestManager?
+    }
+    
+    class var sharedForest:RandomForestManager {
+        return Static.sharedManager!
+    }
+    
+    class func startup() {
+        if (Static.sharedManager == nil) {
+            Static.sharedManager = RandomForestManager()
+        }
+    }
+    
+    init() {
         let path = NSBundle(forClass: self.dynamicType).pathForResource("forest.cv", ofType: nil)
         let cpath = path?.cStringUsingEncoding(NSUTF8StringEncoding)
 
-        _ptr = createRandomForestManager(Int32(sampleSize), Int32(samplingFrequency), UnsafeMutablePointer(cpath!))
+        _ptr = createRandomForestManager(Int32(MotionManager.sampleWindowSize), Int32(1/MotionManager.updateInterval), UnsafeMutablePointer(cpath!))
         self.classCount = Int(randomForestGetClassCount(_ptr))
         self.classLables = [Int32](count:self.classCount, repeatedValue:0)
         randomForestGetClassLabels(_ptr, UnsafeMutablePointer(self.classLables), Int32(self.classCount))
@@ -26,18 +41,35 @@ class RandomForestManager {
     deinit {
         deleteRandomForestManager(_ptr)
     }
+
+    private func magnitudeVector(forSensorData sensorData:NSOrderedSet)->[Float] {
+        var mags: [Float] = []
+        
+        for elem in sensorData {
+            let reading = elem as! SensorData
+            let sum = reading.x.floatValue*reading.x.floatValue + reading.y.floatValue*reading.y.floatValue + reading.z.floatValue*reading.z.floatValue
+            mags.append(sqrtf(sum))
+            if mags.count >= MotionManager.sampleWindowSize { break } // it is possible we over-colleted some of the sensor data
+        }
+        
+        return mags
+    }
     
-    func classifyMagnitudeVector(accelerometerVector: [Float], gyroVector: [Float])->([Int: Float])
+    func classify(sensorDataCollection: SensorDataCollection)
     {
+        let accelVector = self.magnitudeVector(forSensorData: sensorDataCollection.accelerometerAccelerations)
+        let gyroVector = self.magnitudeVector(forSensorData: sensorDataCollection.gyroscopeRotationRates)
+
         let confidences = [Float](count:self.classCount, repeatedValue:0.0)
-        randomForestClassificationConfidences(_ptr, UnsafeMutablePointer(accelerometerVector), UnsafeMutablePointer(gyroVector), UnsafeMutablePointer(confidences), Int32(self.classCount))
+        randomForestClassificationConfidences(_ptr, UnsafeMutablePointer(accelVector), UnsafeMutablePointer(gyroVector), UnsafeMutablePointer(confidences), Int32(self.classCount))
         
         var classConfidences: [Int: Float] = [:]
     
         for (i, score) in confidences.enumerate() {
             classConfidences[Int(classLables[i])] = score
         }
-        
-        return classConfidences
+
+        sensorDataCollection.setActivityTypePredictions(forClassConfidences: classConfidences)
+        CoreDataManager.sharedManager.saveContext()
     }
 }
