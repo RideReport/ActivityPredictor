27138c0dfc92722e77129574e0f050c284ca5a70
Evan Heidtmann <evan.heidtmann@gmail.com>
2017-02-28 15:34:56 -0800

    Use the same hamming window as Apple
    https://developer.apple.com/reference/accelerate/1450040-vdsp_hamm_window

--- FFTManager.cpp
+++ FFTManager.cpp
@@ -17,11 +17,11 @@ struct FFTManager {
 FFTManager *createFFTManager(int sampleSize)
 {
 //    assert(fmod(log2(sampleSize), 1.0) == 0.0); // sampleSize must be a power of 2
-    
+
     struct FFTManager *f;
     f = (struct FFTManager*) malloc(sizeof(struct FFTManager));
     f->fftWeights = vDSP_create_fftsetup(vDSP_Length(log2f(sampleSize)), FFT_RADIX2);
-    
+
     return f;
 }
 
@@ -38,15 +38,14 @@ void fft(FFTManager *manager, float * input, int inputSize, float *output)
     vDSP_hamm_window(hammingWindow, inputSize, 0);
     float *hammedInput = new float[inputSize]();
     vDSP_vmul(input, 1, hammingWindow, 1, hammedInput, 1, inputSize);
-    
     // pack the input samples in preparation for FFT
     float *zeroArray = new float[inputSize]();
     DSPSplitComplex splitComplex = {.realp = hammedInput, .imagp =  zeroArray};
-    
+
     // run the FFT and get the magnitude components (vDSP_zvmags returns squared components)
     vDSP_fft_zip(manager->fftWeights, &splitComplex, 1, log2f(inputSize), FFT_FORWARD);
     vDSP_zvmags(&splitComplex, 1, output, 1, inputSize);
-    
+
     delete[](zeroArray);
     delete[](hammingWindow);
 }
@@ -55,7 +54,7 @@ void autocorrelation(float *input, int inputSize, float *output)
 {
     int lenSignal = 2 * inputSize - 1;
     float *signal = new float[lenSignal];
-    
+
     for (int i = 0; i < inputSize; i++) {
         if (i < inputSize) {
             signal[i] = input[i];
@@ -63,11 +62,11 @@ void autocorrelation(float *input, int inputSize, float *output)
             signal[i] = 0;
         }
     }
-    
+
 //    float *result = new float[inputSize];
 //    vDSP_conv(signal, 1, &input[inputSize - 1], -1, result, 1, inputSize, inputSize);
     vDSP_conv(signal, 1, &input[inputSize - 1], -1, output, 1, inputSize, inputSize);
-    
+
     delete[](signal);
 //    free(result);
 
@@ -83,6 +82,6 @@ float dominantPower(float *input, int inputSize)
             dominantPower = value;
         }
     }
-    
+
     return dominantPower;
 }
